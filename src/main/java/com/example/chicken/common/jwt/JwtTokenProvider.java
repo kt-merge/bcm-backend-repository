ed8481de24b9a4d9e5db1fe06f3d8e29384ba864
Yoojin Lee <yoojinLee.dev@gmail.com>
2025-11-07 17:51:40 +0900

    feat: Update JwtTokenProvider to initialize secret key and expiration time

--- src/main/java/com/example/chicken/common/jwt/JwtTokenProvider.java
+++ src/main/java/com/example/chicken/common/jwt/JwtTokenProvider.java
@@ -1,9 +1,11 @@
 package com.example.chicken.common.jwt;
 
+import io.jsonwebtoken.SignatureAlgorithm;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.spec.SecretKeySpec;
 import java.security.Key;
 
 @Component
@@ -11,12 +13,17 @@ public class JwtTokenProvider {
 
     private String secretKey;
     private int expirationTime;
+    private Key key;
 
     @PostConstruct
     protected void init(@Value("${jwt.secret-key}") String secretKey,
                         @Value("${jwt.expiration-time}") int expirationTime){
+
         this.secretKey = secretKey;
         this.expirationTime = expirationTime;
+
+        new SecretKeySpec(java.util.Base64.getDecoder().decode(secretKey), SignatureAlgorithm.HS512.getJcaName());
     }
+
 
 }
