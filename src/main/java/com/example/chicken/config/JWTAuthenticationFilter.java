5aa282eff4a5ca5505eacbab1d58340cc2fbc3e8
Yoojin Lee <yoojinLee.dev@gmail.com>
2025-11-08 22:39:58 +0900

    feat: Implement token resolution method in JWTAuthenticationFilter

--- src/main/java/com/example/chicken/config/JWTAuthenticationFilter.java
+++ src/main/java/com/example/chicken/config/JWTAuthenticationFilter.java
@@ -4,13 +4,27 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.GenericFilterBean;
 
 import java.io.IOException;
 
 public class JWTAuthenticationFilter extends GenericFilterBean {
+
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
 
     }
+
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer"))
+            return bearerToken.substring(7);
+
+        return null;
+    }
+
 }
