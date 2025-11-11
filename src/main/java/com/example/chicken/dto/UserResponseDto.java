7acdaf9ff33c07d570c06eacfd781d061881d8d1
Yoojin Lee <yoojinLee.dev@gmail.com>
2025-11-11 21:46:52 +0900

    feat: UserResponseDto에 Role, createdAt, modifiedAt 필드 추가

--- src/main/java/com/example/chicken/dto/UserResponseDto.java
+++ src/main/java/com/example/chicken/dto/UserResponseDto.java
@@ -1,10 +1,21 @@
 package com.example.chicken.dto;
 
+import java.time.LocalDateTime;
+
+import com.example.chicken.domain.Role;
 import com.example.chicken.domain.User;
 
-public record UserResponseDto(Long id, String nickname, String email, String phoneNumber) {
+public record UserResponseDto(Long id, String nickname, String email, Role role, String phoneNumber,
+							  LocalDateTime createdAt, LocalDateTime modifiedAt) {
 
-    public static UserResponseDto from(User user) {
-        return new UserResponseDto(user.getId(), user.getNickname(), user.getEmail(), user.getPhoneNumber());
-    }
+	public static UserResponseDto from(User user) {
+		return new UserResponseDto(user.getId(),
+								   user.getNickname(),
+								   user.getEmail(),
+								   user.getRole(),
+								   user.getPhoneNumber(),
+                                   user.getCreatedAt(),
+                                   user.getModifiedAt());
+	}
+
 }
