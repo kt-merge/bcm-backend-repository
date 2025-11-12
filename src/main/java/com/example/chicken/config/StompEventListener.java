ae7b6bcb20f1a2581012a4c86fd149bb9f09d0e4
Yoojin Lee <yoojinLee.dev@gmail.com>
2025-11-12 15:05:09 +0900

    feat: StompEventListener 클래스 추가 및 WebSocket 세션 연결/해제 이벤트 처리

---
+++ src/main/java/com/example/chicken/config/StompEventListener.java
@@ -0,0 +1,30 @@
+package com.example.chicken.config;
+
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class StompEventListener {
+	private final Set<String> sessions = ConcurrentHashMap.newKeySet();
+
+	@EventListener
+	public void connectHandle(SessionConnectEvent event) {
+		StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
+		sessions.add(accessor.getSessionId());
+	}
+
+	@EventListener
+	public void disconnectHandle(SessionDisconnectEvent event) {
+		StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
+		sessions.remove(accessor.getSessionId());
+	}
+}
