d277848e1e9c4219f2d88b891c55abf3a41e4a89
Yoojin Lee <yoojinLee.dev@gmail.com>
2025-11-28 15:45:14 +0900

    feat: Order 엔티티에 FetchType.LAZY 추가 및 Builder 패턴 적용

--- src/main/java/com/example/chicken/domain/order/entity/Order.java
+++ src/main/java/com/example/chicken/domain/order/entity/Order.java
@@ -11,6 +11,7 @@ import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -18,6 +19,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -41,12 +43,27 @@ public class Order extends BaseTimeEntity {
 	@Embedded
 	private ShippingInfo shippingInfo;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "user_id")
 	private User user;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "product_id")
 	private Product product;
+
+	@Builder
+	private Order(OrderStatus status,
+				  BigDecimal finalPrice,
+				  LocalDateTime expiredAt,
+				  ShippingInfo shippingInfo,
+				  User user,
+				  Product product) {
+		this.status = status;
+		this.finalPrice = finalPrice;
+		this.expiredAt = expiredAt;
+		this.shippingInfo = shippingInfo;
+		this.user = user;
+		this.product = product;
+	}
 
 }
