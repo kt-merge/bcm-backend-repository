1a4b43ce2b411b44e584d48826d8bebae5b24ac1
Yoojin Lee <yoojinLee.dev@gmail.com>
2025-11-29 23:09:51 +0900

    feat: OrderResponseDto에 배송 정보 필드 추가

--- src/main/java/com/example/chicken/domain/order/dto/OrderResponseDto.java
+++ src/main/java/com/example/chicken/domain/order/dto/OrderResponseDto.java
@@ -4,14 +4,17 @@ import java.math.BigDecimal;
 
 import com.example.chicken.domain.order.entity.Order;
 import com.example.chicken.domain.order.entity.OrderStatus;
+import com.example.chicken.domain.order.entity.ShippingInfo;
 
-public record OrderResponseDto(Long orderId, String productName, BigDecimal bidPrice, OrderStatus orderStatus) {
+public record OrderResponseDto(Long orderId, String productName, BigDecimal bidPrice, OrderStatus orderStatus,
+							   ShippingInfo shippingInfo) {
 
 	public static OrderResponseDto from(Order order) {
 		return new OrderResponseDto(order.getId(),
 									order.getProduct().getName(),
 									order.getFinalPrice(),
-									order.getStatus());
+									order.getStatus(),
+									order.getShippingInfo());
 	}
 
 }
