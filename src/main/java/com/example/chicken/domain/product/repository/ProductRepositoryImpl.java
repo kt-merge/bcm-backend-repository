2007457085c9cfef08a675580ddedb8911e30c40
Yoojin Lee <yoojinLee.dev@gmail.com>
2025-12-12 11:34:57 +0900

    feat(QueryDSL): Product 검색 조건 추가
    - ID 검색 기능 추가
    - 이름 검색 기능 추가
    - Category ID 검색 기능 추가
    - 경매 상태 추가

--- src/main/java/com/example/chicken/domain/product/repository/ProductRepositoryImpl.java
+++ src/main/java/com/example/chicken/domain/product/repository/ProductRepositoryImpl.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.support.PageableExecutionUtils;
 
 import com.example.chicken.domain.product.dto.ProductSearchCondition;
+import com.example.chicken.domain.product.entity.BidStatus;
 import com.example.chicken.domain.product.entity.Product;
 import com.querydsl.core.types.Predicate;
 import com.querydsl.jpa.impl.JPAQuery;
@@ -24,13 +25,19 @@ public class ProductRepositoryImpl implements ProductRepositoryCustom {
 	@Override
 	public Page<Product> searchProducts(ProductSearchCondition condition, Pageable pageable) {
 
+		Long id = condition.id();
 		String name = condition.name();
+		Long categoryId = condition.categoryId();
+		BidStatus bidStatus = condition.bidStatus();
 
 		List<Product> result = queryFactory
 			.select(product)
 			.from(product)
 			.where(
-				hasNameContaining(name)
+				equalsId(id),
+				hasNameContaining(name),
+				equalsCategoryId(categoryId),
+				equalsBidStatus(bidStatus)
 			)
 			.offset(pageable.getOffset())
 			.limit(pageable.getPageSize() + 1L)
@@ -40,14 +47,29 @@ public class ProductRepositoryImpl implements ProductRepositoryCustom {
 			.select(product.count())
 			.from(product)
 			.where(
-				hasNameContaining(name)
+				equalsId(id),
+				hasNameContaining(name),
+				equalsCategoryId(categoryId),
+				equalsBidStatus(bidStatus)
 			);
 
 		return PageableExecutionUtils.getPage(result, pageable, countQuery::fetchOne);
 	}
 
-	private static Predicate hasNameContaining(String name) {
+	private Predicate equalsId(Long id) {
+		return id != null ? product.id.eq(id) : null;
+	}
+
+	private Predicate hasNameContaining(String name) {
 		return name != null ? product.name.contains(name) : null;
+	}
+
+	private Predicate equalsCategoryId(Long categoryId) {
+		return categoryId != null ? product.category.id.eq(categoryId) : null;
+	}
+
+	private Predicate equalsBidStatus(BidStatus bidStatus) {
+		return bidStatus != null ? product.bidStatus.eq(bidStatus) : null;
 	}
 
 }
