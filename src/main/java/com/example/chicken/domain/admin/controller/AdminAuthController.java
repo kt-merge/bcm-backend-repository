fc1cd81430caa163726dfac0fd918e5481a64a9a
Yoojin Lee <yoojinLee.dev@gmail.com>
2025-12-18 10:05:41 +0900

    hotfix: AdminAuthController 쿠키 반환

--- src/main/java/com/example/chicken/domain/admin/controller/AdminAuthController.java
+++ src/main/java/com/example/chicken/domain/admin/controller/AdminAuthController.java
@@ -3,11 +3,18 @@ package com.example.chicken.domain.admin.controller;
 import static com.example.chicken.common.constant.PathConstant.Admin.ADMIN_AUTH_PREFIX;
 import static com.example.chicken.common.constant.PathConstant.Auth.SIGN_IN;
 
+import com.example.chicken.common.constant.CookieConstant;
+import com.example.chicken.common.util.CookieUtil;
 import com.example.chicken.domain.auth.dto.auth.SignInRequestDto;
 import com.example.chicken.domain.auth.dto.auth.SignInResponseDto;
+import com.example.chicken.domain.auth.dto.user.AccessTokenResponseDto;
 import com.example.chicken.domain.auth.service.AuthService;
 import jakarta.validation.Valid;
+import java.time.Duration;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,11 +29,23 @@ public class AdminAuthController {
     private final AuthService authService;
 
     @PostMapping(SIGN_IN)
-    public ResponseEntity<SignInResponseDto> signInAdmin(@RequestBody @Valid SignInRequestDto request) {
+    public ResponseEntity<AccessTokenResponseDto> signInAdmin(@RequestBody @Valid SignInRequestDto request) {
 
-        SignInResponseDto result = this.authService.signInAdmin(request);
+        SignInResponseDto tokens = this.authService.signInAdmin(request);
 
-        return ResponseEntity.ok(result);
+        Duration cookieDuration = Boolean.TRUE.equals(request.rememberMe())
+                ? Duration.ofDays(15) : Duration.ofSeconds(-1);
+
+        ResponseCookie cookie = CookieUtil.generateCookieResponse(CookieConstant.REFRESH_TOKEN_NAME,
+                tokens.refreshToken(),
+                cookieDuration);
+
+        AccessTokenResponseDto result = AccessTokenResponseDto.from(tokens.accessToken());
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .body(result);
     }
 
 }
